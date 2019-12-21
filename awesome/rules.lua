b8535cbd6fc64021bc03b839901b0ea446be076c
Alexey Shmakov <razzeeyy@gmail.com>
2019-12-21 19:46:45 +0300

    unmaximize windows on awesome startup

--- awesome/rules.lua
+++ awesome/rules.lua
@@ -16,6 +16,7 @@ return function (clientkeys, clientbuttons)
                 screen = awful.screen.preferred,
                 placement = awful.placement.no_overlap+awful.placement.no_offscreen,
                 size_hints_honor = false,
+                maximized = false,
                 maximized_vertical   = false,
                 maximized_horizontal = false,
             }
