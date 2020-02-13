f019ec3e9c6d84971b50cfb475b1eda9fdd86de4
Alexey Shmakov <razzeeyy@gmail.com>
2020-02-14 01:29:11 +0300

    vscode fonts

--- install-deps.sh
+++ install-deps.sh
@@ -1,1 +1,2 @@
 sudo apt install scrot xclip arandr pavucontrol volumeicon-alsa i3lock compton
+sudo apt install fonts-hack
---
+++ ln-vscode.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/.config/Code/User/
+ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
