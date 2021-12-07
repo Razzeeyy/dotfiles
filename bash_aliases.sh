5b7409c012f299c12c9e5eff5c718a3910a6f42c
Alexey Shmakov <razzeeyy@gmail.com>
2021-12-07 19:13:40 +0300

    update to aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -6,9 +6,10 @@ parse_git_branch() {
 #           \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$
 export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[0;31m\]\$(parse_git_branch)\[\033[00m\]\$ "
 
-alias git_remove_merged_branches='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
+alias git_remove_merged_branches='git branch --merged | egrep -v "(^\*|master|release|dev)" | xargs git branch -d'
 alias git_remove_deleted_branches='git remote prune origin'
 
 alias git_log='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 
-alias git_pull='git checkout master && git pull && git checkout release && git pull'
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
